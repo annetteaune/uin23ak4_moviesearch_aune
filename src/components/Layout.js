47d949b406c75736c35972e87c7a9566b7cdbd73
annetteaune <annetteaune@outlook.com>
2023-03-06 12:28:16 +0100

    Opprettet basic layout + css linket

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+export default function Layout() {
+	return (
+		<>
+			<header>
+				<h1>Filmsøk</h1>
+			</header>
+			<main>
+                <span>Her skal det være en liste med filmkort</span>
+            </main>
+            <footer><span>Footer</span></footer>
+		</>
+	);
+}
