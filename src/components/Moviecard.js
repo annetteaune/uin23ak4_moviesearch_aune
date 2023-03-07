872b32e25d44c7663277fafcf5fde2c0040b3789
annetteaune <annetteaune@outlook.com>
2023-03-07 10:01:14 +0100

    css - moviecards

--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -1,18 +1,39 @@
-export default function Moviecard({title,year, genre, director, actors, awards, plot, image}) {
-	
+export default function Moviecard({
+	title,
+	year,
+	genre,
+	director,
+	actors,
+	awards,
+	plot,
+	image,
+}) {
 	return (
 		<article>
-			<img className="poster"
-				src={image}
-				alt=""
-			/>
-			<h3 className="movie-title">{title}</h3>
-			<span className="year">({year})</span>
-            <span className="plot">{plot}</span>
-			<span className="genre">Genre: {genre}</span>
-			<span className="director">Director: {director}</span>
-			<span className="actors">Actors: {actors}</span>
-			<span className="awards">Awards: {awards}</span>
+			<img className="poster" src={image} alt="" />
+
+			<div className="article-container">
+				<h3 className="movie-title">{title}</h3>
+				<span className="card-info">({year})</span>
+				<span>{plot}</span>
+				<div>
+					<span className="card-info">Genre:</span>{" "}
+					<span className="card-value">{genre}</span>
+				</div>
+				<div>
+					<span className="card-info">Director:</span>{" "}
+					<span className="card-value">{director}</span>
+				</div>
+				<div>
+					<span className="card-info">Actors: </span>
+					<span className="card-value">{actors}</span>
+				</div>
+
+				<div>
+					<span className="card-info">Awards:</span>{" "}
+					<span className="card-value">{awards}</span>
+				</div>
+			</div>
 		</article>
 	);
 }
