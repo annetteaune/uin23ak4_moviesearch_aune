876f3c60c611be40358c8e87402a7b54fcf01def
annetteaune <annetteaune@outlook.com>
2023-03-09 22:05:41 +0100

    moviecard + css updates

--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -10,30 +10,25 @@ export default function Moviecard({
 }) {
 	return (
 		<article>
-			<img className="poster" src={image} alt="" />
-
+			<h3 className="movie-title">
+				{title}
+				<span className="card-info"> ({year})</span>
+			</h3>
 			<div className="article-container">
-				<h3 className="movie-title">{title}</h3>
-				<span className="card-info">({year})</span>
-				<span className="plot">{plot}</span>
-				<div>
-					<span className="card-info">Genre:</span>{" "}
-					<span className="card-value">{genre}</span>
-				</div>
-				<div>
-					<span className="card-info">Director:</span>{" "}
-					<span className="card-value">{director}</span>
-				</div>
-				<div>
-					<span className="card-info">Actors: </span>
-					<span className="card-value">{actors}</span>
-				</div>
+				<img className="poster" src={image} alt="" />
 
-				<div>
-					<span className="card-info">Awards:</span>{" "}
-					<span className="card-value">{awards}</span>
-				</div>
+				<ul>
+					<li className="card-info">Genre:</li>
+					<li className="card-value">{genre}</li>
+					<li className="card-info">Director:</li>
+					<li className="card-value">{director}</li>
+					<li className="card-info">Actors: </li>
+					<li className="card-value">{actors}</li>
+					<li className="card-info">Awards:</li>
+					<li className="card-value">{awards}</li>
+				</ul>
 			</div>
+			<span className="plot">{plot}</span>
 		</article>
 	);
 }
